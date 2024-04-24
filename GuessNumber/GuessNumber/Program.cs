a5c111fa4f2d6bf62c6452eed5b6fa501b6e24c9
LenkaSvob <LenkaLiberty@gmail.com>
2024-04-24 20:08:02 +0200

    Created GuessNumber program

---
+++ GuessNumber/GuessNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace GuessNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = new Random().Next(1, 10 + 1);
+            Console.Write("Guess the number I'm thinking of: ");
+            bool isNum = int.TryParse(Console.ReadLine(), out int guess);
+
+            if (myNum < guess)
+            {
+                Console.WriteLine("My number is smaller, guess again:");
+            }
+            else if (myNum > guess)
+            {
+                Console.WriteLine("My number is bigger, guess again:");
+            }
+            else if (myNum == guess)
+            {
+                Console.WriteLine("That's the right number!");
+            }
+            else if (isNum == false)
+            {
+                Console.WriteLine("My number is bigger, guess again:");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
