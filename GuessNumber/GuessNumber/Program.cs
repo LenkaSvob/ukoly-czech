077d10bf0a7bd2f4c5b59b32b5b6f60d6d01f888
LenkaSvob <LenkaLiberty@gmail.com>
2024-04-24 20:15:25 +0200

    Added while loop

--- GuessNumber/GuessNumber/Program.cs
+++ GuessNumber/GuessNumber/Program.cs
@@ -6,23 +6,28 @@
         {
             int myNum = new Random().Next(1, 10 + 1);
             Console.Write("Guess the number I'm thinking of: ");
-            bool isNum = int.TryParse(Console.ReadLine(), out int guess);
 
-            if (myNum < guess)
+            while(true)
             {
-                Console.WriteLine("My number is smaller, guess again:");
-            }
-            else if (myNum > guess)
-            {
-                Console.WriteLine("My number is bigger, guess again:");
-            }
-            else if (myNum == guess)
-            {
-                Console.WriteLine("That's the right number!");
-            }
-            else if (isNum == false)
-            {
-                Console.WriteLine("My number is bigger, guess again:");
+                bool isNum = int.TryParse(Console.ReadLine(), out int guess);
+
+                if (myNum < guess)
+                {
+                    Console.WriteLine("My number is smaller, guess again:");
+                }
+                else if (myNum > guess)
+                {
+                    Console.WriteLine("My number is bigger, guess again:");
+                }
+                else if (myNum == guess)
+                {
+                    Console.WriteLine("That's the right number!");
+                    break;
+                }
+                else if (isNum == false)
+                {
+                    Console.WriteLine("My number is bigger, guess again:");
+                }
             }
 
             Console.ReadLine();
