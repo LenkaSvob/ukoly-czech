ea5121472810f8ea3d7f56dc5f39c42fea1be185
LenkaSvob <LenkaLiberty@gmail.com>
2024-04-24 19:37:41 +0200

    Created Asterisks program

---
+++ Asterisks/Asterisks/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Asterisks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter amount of stars: ");
+
+
+            bool isNum = int.TryParse(Console.ReadLine(), out int starsNum);
+            if (isNum == true)
+            {
+                for (int i = 0; i < starsNum; i++)
+                {
+                    Console.Write("*");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Enter a valid number.");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
