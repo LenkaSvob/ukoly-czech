97ca72eb1b61f1baee7efe2cf2ac46291a92be7c
LenkaSvob <LenkaLiberty@gmail.com>
2024-04-24 20:47:21 +0200

    Created Archer program

---
+++ Archer/Archer/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Archer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrowsToShoot = 10;
+
+            Archer archer = new Archer(arrowsToShoot);
+
+            for (int i = 0; i < arrowsToShoot; i++)
+            {
+                archer.Shoot();
+            }
+            archer.Shoot(); // now no arrows left
+
+            Console.ReadLine();
+        }
+    }
+    class Archer
+    {
+        public int NumOfArrows { get; set; }
+
+        public Archer(int totalArrow)
+        {
+            this.NumOfArrows = totalArrow;
+        }
+        public void Shoot()
+        {
+            if (NumOfArrows > 0)
+            {
+                Console.WriteLine("I always hit right in the middle!");
+                NumOfArrows--;
+            }
+            else if (NumOfArrows == 0)
+            {
+                Console.WriteLine("No arrows left");
+            }
+        }
+    }
+}
+
